fab38ddc562e5033a52ac963c2987679e2e8552a
Tomáš Erlebach <t.erleb@seznam.cz>
2024-11-02 21:26:09 +0100

    Vlastnosti a metody

---
+++ 08-OOP/Kruh.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_OOP
+{
+    internal class Kruh
+    {
+
+        // Atribut poloměru
+        private double _polomer;
+
+        // Vlastnost poloměru
+        public double Polomer
+        {
+            get { return _polomer; }
+
+            set
+            {
+                if (value > 0)
+                {
+                    _polomer = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Poloměr musí být kladné číslo.");
+                }
+            }
+
+        }
+        // Konstruktor s volitelným parametrem poloměru
+        public Kruh(double polomer = 1.0)
+        {
+            Polomer = polomer;
+        }
+
+        // Vlastnost pouze pro výpočet obsahu
+        public double Obsah
+        {
+            get { return Math.PI * _polomer * _polomer; }
+        }
+
+        // Metoda pro změnu poloměru (mění stav objektu)
+        public void ZmenitVelikost(double koeficient)
+        {
+            _polomer *= koeficient;
+        }        
+       
+        // funkce pro výpočet obvodu
+        public double VypocitatObvod(double polomer)
+        {
+            return 2 * Math.PI * polomer;
+        }
+
+        public double Obvod
+        {
+            get { return VypocitatObvod(_polomer); }
+        }       
+    }
+}
---
+++ 08-OOP/Program.cs
@@ -0,0 +1,6 @@
+﻿using _08_OOP;
+
+Kruh kruh = new Kruh(5);
+// Výpočet a zobrazení obsahu kruhu kruh1
+double obsah = kruh.Obsah;
+Console.WriteLine($"Obsah kruhu s poloměrem {kruh.Polomer} je {obsah}");
