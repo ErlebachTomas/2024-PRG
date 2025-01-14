e2a8393b608fe0a8d9d9a28536872089a3e3aac8
Tomáš Erlebach <t.erleb@seznam.cz>
2025-01-14 09:15:34 +0100

    Pretizeni operatoru

--- 14-Farma/Models/Tiger.cs
+++ 14-Farma/Models/Tiger.cs
@@ -11,7 +11,7 @@
 
         public override string ToString()
         {
-            return $"Tygr {Id}";
+            return "Tygr " + Id;
         }
 
         public double Consume(Animal animal)
